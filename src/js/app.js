0404982e6b60aa1bd7568011470a55ec9cef94e8
shahrizoda <shahrizodadeveloping@gmail.com>
2022-08-20 14:44:10 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,24 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation} from 'swiper';
 
-const swiper = new Swiper();
+const swiper1 = new Swiper();
 
-var Swiper = new Swiper(".products__swiper", {
-  slidesPerView: 3,
-  spaceBetween: 37,
+var swiper = new Swiper(".products__swiper", {
+  modules: [Navigation],
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: true,
+  breakpoints:{
+    1200:{
+      slidesPerView: 'auto',
+      spaceBetween: 37,
+      initialSlide: 2,
+      centeredSlides: true,
+    },
+  },
 });
 
 const menu = document.querySelector('.menu');
@@ -18,5 +29,5 @@ const body = document.querySelector('body');
 menu.addEventListener('click', (e)=>{
     menu.classList.toggle('active')
     body.classList.toggle('hidden')
-    headerRight.classList.toggle('active')
+    headerRight.classList.toggle('active') 
 })
